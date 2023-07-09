c0aac52ab126b9beb95cd8d67ca5fc84a754f701
Przemysław Lewandowski <p4lewandowski@gmail.com>
2023-07-09 11:31:51 +0200

    Added Config class

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from config import Config
 
 # instance of Flask application defined in app package accessible as `from app import app`
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
