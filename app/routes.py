bf80294a6aff9dbf6a97c6855c9e9cc1e44bcf6c
Przemysław Lewandowski <p4lewandowski@gmail.com>
2023-07-09 11:09:22 +0200

    Development flags and templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username' : 'Carlos'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'What a fancy blog!'
+        },
+        {
+            'author': {'username': 'Brad'},
+            'body': 'Yea, I like it too!'
+        }
+    ]
+    return render_template('index.html', title='Home', posts=posts, user=user)
+    
