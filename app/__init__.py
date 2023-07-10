f81ad474fdd9ec94720780b11650c9f0751d0d59
Przemysław Lewandowski <p4lewandowski@gmail.com>
2023-07-10 20:20:56 +0200

    Created the database as well as database migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # instance of Flask application defined in app package accessible as `from app import app`
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+# includes the files from the main app module
+from app import routes, models
